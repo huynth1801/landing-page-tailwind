b17da2495862f671c760fc9edcd2cb00e6e8d5c0
huynth1801 <huuhuy1801@gmail.com>
2023-02-21 11:30:46 +0700

    add landing page tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      height: {
+        custom: "620px"
+      },
+      spacing: {
+        "40-c": "40rem"
+      }
+    },
+  },
+  plugins: [],
+}
